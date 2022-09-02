6963e62924ae01f839322d41959c8badb4b41bef
Vikram Kangotra <vikramkangotra8055@gmail.com>
2022-09-02 21:45:28 +0530

    handles type converion

--- src/lexer.cpp
+++ src/lexer.cpp
@@ -143,7 +143,109 @@ Token Lexer::identifier() {
     while (isalpha(peek()) || isdigit(peek())) {
         advance();
     }
-    return Token(TokenType::Identifier, input.substr(start, current - start));
+    std::string str = input.substr(start, current - start);
+
+    switch (str[0]) {
+        case 'a': {
+            if (str == "and") {
+                return Token(TokenType::And, str);
+            }
+        }; break;
+        case 'b': {
+            if (str == "break") {
+                return Token(TokenType::Break, str);
+            }
+        }; break;
+        case 'c': {
+            if (str == "class") {
+                return Token(TokenType::Class, str);
+            } else if (str == "continue") {
+                return Token(TokenType::Continue, str);
+            }
+        }; break;
+        case 'd': {
+            if (str == "do") {
+                return Token(TokenType::Do, str);
+            }
+        }; break;
+        case 'e': {
+            if (str == "else") {
+                return Token(TokenType::Else, str);
+            } else if (str == "elseif") {
+                return Token(TokenType::ElseIf, str);
+            }
+        }; break;
+        case 'f': {
+            if (str == "false") {
+                return Token(TokenType::False, str);
+            } else if (str == "for") {
+                return Token(TokenType::For, str);
+            } else if (str == "fun") {
+                return Token(TokenType::Fun, str);
+            }
+        }; break;
+        case 'i': {
+            if (str == "if") {
+                return Token(TokenType::If, str);
+            } else if (str == "in") {
+                return Token(TokenType::In, str);
+            }
+        }; break;
+        case 'l': {
+            if (str == "let") {
+                return Token(TokenType::Let, str);
+            }
+        }; break;
+        case 'n': {
+            if (str == "nil") {
+                return Token(TokenType::Nil, str);
+            }
+        }; break;
+        case 'o': {
+            if (str == "or") {
+                return Token(TokenType::Or, str);
+            }
+        }; break;
+        case 'p': {
+            if (str == "print") {
+                return Token(TokenType::Print, str);
+            }
+        }; break;
+        case 'r': {
+            if (str == "return") {
+                return Token(TokenType::Return, str);
+            }
+        }; break;
+        case 's': {
+            if (str == "super") {
+                return Token(TokenType::Super, str);
+            } else if (str == "static") {
+                return Token(TokenType::Static, str);
+            } else if (str == "struct") {
+                return Token(TokenType::Struct, str);
+            } else if (str == "switch") {
+                return Token(TokenType::Switch, str);
+            }
+        }; break;
+        case 't': {
+            if (str == "true") {
+                return Token(TokenType::True, str);
+            } else if (str == "this") {
+                return Token(TokenType::This, str);
+            }
+        }; break;
+        case 'v': {
+            if (str == "var") {
+                return Token(TokenType::Var, str);
+            }
+        }; break;
+        case 'w': {
+            if (str == "while") {
+                return Token(TokenType::While, str);
+            }
+        }; break;
+    }
+    return Token(TokenType::Identifier, str);
 }
 
 Token Lexer::number() {
@@ -158,7 +260,8 @@ Token Lexer::number() {
         }
     }
 
-    return Token(TokenType::Number, input.substr(start, current - start));
+    std::string str = input.substr(start, current - start);
+    return Token(TokenType::Number, str, std::stof(str) );
 }
 
 Token Lexer::string() {
@@ -172,5 +275,6 @@ Token Lexer::string() {
         return Token(TokenType::Unknown, "Unterminated string");
     }
     advance();
-    return Token(TokenType::String, input.substr(start + 1, current - start - 2));
+    std::string str = input.substr(start + 1, current - start - 2);
+    return Token(TokenType::String, str, str);
 }
