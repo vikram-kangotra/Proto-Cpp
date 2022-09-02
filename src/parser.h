f230b5ce0a9e57281b7882219c21a66a28c2dcc1
Vikram Kangotra <vikramkangotra8055@gmail.com>
2022-09-02 21:50:36 +0530

    added functions for stmt

--- src/parser.h
+++ src/parser.h
@@ -3,6 +3,7 @@
 #include <memory>
 #include <vector>
 #include "token.h"
+#include <iostream>
 #include "expr.h"
 
 class Parser {
@@ -10,7 +11,7 @@ class Parser {
         Parser(const std::vector<Token>& tokens)
         : tokens{tokens} {}
 
-        std::unique_ptr<Expr> parse();
+        std::vector<std::unique_ptr<Stmt>> parse();
 
     private:
         bool atEnd();
@@ -18,10 +19,16 @@ class Parser {
         Token advance();
         Token previous();
         bool eat(TokenType type);
+        void consume(TokenType type, const std::string& err);
         bool check(TokenType type);
         bool match(TokenType type);
 
+        std::unique_ptr<Stmt> statement();
+        std::unique_ptr<Stmt> expressionStatement();
+        std::unique_ptr<Stmt> printStatement();
         std::unique_ptr<Expr> expr();
+        std::unique_ptr<Expr> equality();
+        std::unique_ptr<Expr> comparison();
         std::unique_ptr<Expr> term();
         std::unique_ptr<Expr> factor();
         std::unique_ptr<Expr> unary();
