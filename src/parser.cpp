a10c3ce9016505cb0e03f45accd74172be061c2e
Vikram Kangotra <vikramkangotra8055@gmail.com>
2022-09-02 21:50:59 +0530

    added stmt function implementation

--- src/parser.cpp
+++ src/parser.cpp
@@ -2,14 +2,20 @@
 #include <exception>
 #include <iostream>
 
-// expr     -> term
-// term     -> factor (("-" | "+") factor)*;
-// factor   -> unary (("*" | "/") unary)*;
-// unary    -> ("+" | "-") unary | primary;
-// primary  -> num | "(" expr ")";
+// program      -> statement* EOF;
+// statement    -> exprStmt | printStmt;
+// exprStmt     -> expr ";";
+// printStmt    -> "print" expr ";";
+// expr         -> equality
+// equality     -> comparison (("!=" | "==") comparison)*;
+// comparison   -> term ((">" | ">=" | "<" | "<=") term)*;
+// term         -> factor (("-" | "+") factor)*;
+// factor       -> UnaryExpr (("*" | "/") UnaryExpr)*;
+// UnaryExpr        -> ("+" | "-") UnaryExpr | primary;
+// primary      -> num | "(" expr ")";
 
 bool Parser::atEnd() {
-    return peek().type == TokenType::Eof;
+    return current >= tokens.size() || peek().type == TokenType::Eof;
 }
 
 Token Parser::peek() {
@@ -27,12 +33,9 @@ Token Parser::previous() {
     return tokens[current - 1];
 }
 
-bool Parser::eat(TokenType type) {
-    if (peek().type == type) {
-        advance();
-        return true;
-    }
-    return false;
+void Parser::consume(TokenType type, const std::string& err) {
+    if (!match(type))
+        throw std::runtime_error(err);
 }
 
 bool Parser::check(TokenType type) {
@@ -47,12 +50,53 @@ bool Parser::match(TokenType type) {
     return false;
 }
 
-std::unique_ptr<Expr> Parser::parse() {
-    return expr();
+std::vector<std::unique_ptr<Stmt>> Parser::parse() {
+    std::vector<std::unique_ptr<Stmt>> statements;
+    while (!atEnd()) {
+        statements.push_back(statement());
+    }
+    return statements;
+}
+
+std::unique_ptr<Stmt> Parser::statement() {
+    if (match(TokenType::Print)) return printStatement();
+    return expressionStatement();
+}
+
+std::unique_ptr<Stmt> Parser::printStatement() {
+    std::unique_ptr<Expr> value = expr();
+    consume(TokenType::Semicolon, "Expect ';' after value.'");
+    return std::make_unique<PrintStmt>(std::move(value));
+}
+
+std::unique_ptr<Stmt> Parser::expressionStatement() {
+    std::unique_ptr<Expr> expression = expr();
+    consume(TokenType::Semicolon, "Expect ';' afer expression");
+    return std::make_unique<ExpressionStmt>(std::move(expression));
 }
 
 std::unique_ptr<Expr> Parser::expr() {
-    return term();
+    return equality();
+}
+
+std::unique_ptr<Expr> Parser::equality() {
+    auto left = comparison();
+    while (match(TokenType::BangEqual) || match(TokenType::EqualEqual)) {
+        auto op = previous();
+        auto right = comparison();
+        left = std::make_unique<BinaryExpr>(std::move(left), op, std::move(right));
+    }
+    return left;
+}
+
+std::unique_ptr<Expr> Parser::comparison() {
+    auto left = term();
+    while (match(TokenType::Greater) || match(TokenType::GreaterEqual) || match(TokenType::Less) || match(TokenType::LessEqual)) {
+        auto op = previous();
+        auto right = term();
+        left = std::make_unique<BinaryExpr>(std::move(left), op, std::move(right));
+    }
+    return left;
 }
 
 std::unique_ptr<Expr> Parser::term() {
@@ -60,7 +104,7 @@ std::unique_ptr<Expr> Parser::term() {
     while (match(TokenType::Plus) || match(TokenType::Minus)) {
         Token op = previous();
         std::unique_ptr<Expr> right = factor();
-        left = std::make_unique<Binary>(std::move(left), op, std::move(right));
+        left = std::make_unique<BinaryExpr>(std::move(left), op, std::move(right));
     }
     return left;
 }
@@ -70,7 +114,7 @@ std::unique_ptr<Expr> Parser::factor() {
     while (match(TokenType::Star) || match(TokenType::Slash)) {
         Token op = previous();
         std::unique_ptr<Expr> right = unary();
-        left = std::make_unique<Binary>(std::move(left), op, std::move(right));
+        left = std::make_unique<BinaryExpr>(std::move(left), op, std::move(right));
     }
     return left;
 }
@@ -79,24 +123,27 @@ std::unique_ptr<Expr> Parser::unary() {
     if (match(TokenType::Plus) || match(TokenType::Minus)) {
         Token op = previous();
         std::unique_ptr<Expr> expr = unary();
-        return std::make_unique<Unary>(op, std::move(expr));
+        return std::make_unique<UnaryExpr>(op, std::move(expr));
     }
-    if (eat(TokenType::LeftParen)) {
+    if (match(TokenType::LeftParen)) {
         std::unique_ptr<Expr> exp = expr();
-        eat(TokenType::RightParen);
+        consume(TokenType::RightParen, "Expected ')'");
         return exp;
     }
     return primary();
 }
 
 std::unique_ptr<Expr> Parser::primary() {
-    if (match(TokenType::Number)) {
-        return std::make_unique<Literal>(previous());
+    if (match(TokenType::True)) return std::make_unique<LiteralExpr>(false);
+    if (match(TokenType::True)) return std::make_unique<LiteralExpr>(true);
+
+    if (match(TokenType::Number) || match(TokenType::String)) {
+        return std::make_unique<LiteralExpr>(previous().literal);
     }
     if (match(TokenType::LeftParen)) {
-        std::unique_ptr<Expr> expr = parse();
+        std::unique_ptr<Expr> exp = expr();
         match(TokenType::RightParen);
-        return expr;
+        return exp;
     }
-    throw std::runtime_error("Unexpected token: " + peek().lexeme);
+    throw std::runtime_error("Parsing Error - Unexpected token: " + peek().lexeme);
 }
